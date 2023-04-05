fed4947b56e9e72ce12b13705e7a9cb4513af25f
Gangula Sandaru Dinusantha <gangula.sandaru@gmail.com>
2023-04-05 22:47:19 +0530

    remove unnessary data

--- PDFExtractor/settings.py
+++ PDFExtractor/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nlo^18hv^mh4vn5-_2r-z+9%0vfek*ydwsdju8g6aacya(vy58'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
